68ddcfed7ddc5c322b468f54fefea2f2c9d0bc3f
Ravindra Verma <104000933+Ravindra504@users.noreply.github.com>
2022-11-22 12:43:57 +0530

    Add files via upload

---
+++ SwitchExample.java
@@ -0,0 +1,36 @@
+package Switch;
+import java.util.Scanner;
+public class SwitchExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Enter the operation to perform:-");
+		Scanner sc=new Scanner(System.in);
+		char operation=sc.next().charAt(0);
+		System.out.println("Enter the number:-");
+		int num1=sc.nextInt();
+		int num2=sc.nextInt();
+		
+		switch(operation) 
+		{
+		case'+':
+			System.out.println(num1+num2);
+			break;
+		case'-':
+			System.out.println(num1-num2);
+			break;
+		case'*':
+			System.out.println(num1*num2);
+			break;
+		case'/':
+			System.out.println(num1/num2);
+			break;
+		case'%':
+			System.out.println(num1%num2);
+			break;
+			default:
+				System.out.println("Invalid input");
+		}
+	}
+
+}
